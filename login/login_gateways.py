ae48fe3813eb38e84cbde0e8a5869c3939e8c910
Siris <sirishsilpakar@gmail.com>
2019-06-08 12:28:57 +0545

    Login Gateway concrete classes were introduced

---
+++ login/login_gateways.py
@@ -0,0 +1,23 @@
+from login.login_abstract import LoginGateway
+
+
+class GoogleLogin(LoginGateway):
+    """This class should have the necessary properties and methods to 
+        process the login through a Google account. """
+
+    def processLogin(self):
+        return "Google Login method"
+
+
+class FacebookLogin(LoginGateway):
+    """This class should have the necessary properties and methods to 
+        process the login through a Facebook account. """
+    def processLogin(self):
+        return "Facebook Login method"
+
+
+class EmailLogin(LoginGateway):
+    """This class should have the necessary properties and methods to 
+        process the login through an email address. """
+    def processLogin(self):
+        return "Email Login method"
