8a0c5a9cd77322e190900ee4b5a5a8e3fddc0498
Siris <sirishsilpakar@gmail.com>
2019-06-08 12:16:50 +0545

    Added the interface class for payment gateways

---
+++ login/login_abstract.py
@@ -0,0 +1,15 @@
+# Python on its own doesn't provide abstract class. It comes with 'abc' module that provides the basis for defining Abstract Base Classes
+# This modules also provides the decorators for defining a class method as abstract method.
+from abc import ABC, abstractmethod
+
+class LoginGateway(ABC):
+""" This is an interface that provides the sub classes will inherit from,
+    to implement the login methods for the login gateways defined. """
+
+
+    def __init__(self):
+        super().__init__()
+
+
+    def processLogin(self):
+        pass
